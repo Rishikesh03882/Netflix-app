b19917e575a9fe4d65b7ed7a97db51fe2673e3e9
Rushikesh Suryawanshi <rushisuryawanshi146@gmail.com>
2025-04-10 11:23:16 +0530

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,7 +3,6 @@ import './Home.css'
 import Navbar from '../../Components/Navbar/Navbar'
 import hero1_banner from '../../assets/hero1_banner.jpg'
 import hero_title from '../../assets/hero_title.png'
-import play_icon from '../../assets/play_icon.png'
 import info_icon from '../../assets/info_icon.png'
 import TitleCards from '../../Components/TitleCards/TitleCards'
 import Footer from '../../Components/Footer/Footer'
@@ -17,8 +16,7 @@ const Home = () => {
                 <div className="hero-caption">
                     <img src={hero_title} alt="" className='caption-img' />
                     <p>Discovering his ties to a secret ancient order, a young man living in modern Istanbul embarks on a quest to save the city from an immortal enemy</p>
-                    <div className="hero-btns">
-                        <button className='btn'><img src={play_icon} alt="" />Play</button>
+                    <div className="hero-btns"
                         <button className='btn dark-btn'><img src={info_icon} alt="" />More Info</button>
                     </div>
                 </div>
